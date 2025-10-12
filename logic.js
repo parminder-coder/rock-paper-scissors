61bc146ec283c788c7bb06a4a6c5ab0d73bfcdc6
parminder-coder <singhparminder0602@gmail.com>
2025-10-12 17:30:07 +0530

    added a bsic ui

--- logic.js
+++ logic.js
@@ -1,6 +1,11 @@
-let human=0;
-let computer=0;
-let choice=["rock","paper","scissors"];
+let human_score=0;
+let computer_score=0;
+const vs=document.getElementById("vs");
+const choice=["rock","paper","scissors"];
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 function random_index(){
     return(Math.floor(Math.random()*3));
@@ -13,25 +18,44 @@ function human_choice(){
     return prompt("enter rock , paper or scissors");
 }
 
+function changing_images(computer_choice,human_choice){
+    const human_img=document.getElementById('user_image');
+    const computer_img=document.getElementById('computer_image');
+
+    const h_image_name=document.getElementById('user_image_name');
+    const c_image_name=document.getElementById('computer_image_name');
+
+    computer_img.innerHTML = `<img src="images/${computer_choice}.png" alt="${computer_choice}">`;
+    human_img.innerHTML=`<img src="images/${human_choice}.png" alt="${human_choice}">`;
+
+    h_image_name.innerText=`${human_choice}`
+    c_image_name.innerText=`${computer_choice}`
+}
+
 function play_game(computer_choice,human_choice){
+
+    
 
     if(computer_choice==="rock"){
 
         if(human_choice=="rock"){
             console.log("rock and rock");
             console.log("match tied");
+            vs.innerText="tie"
         }
 
-        else if(human_choice="paper"){
+        else if(human_choice==="paper"){
             console.log("paper beats rock");
             console.log("you won");
-            human+=1;
+            vs.innerText="beats"
+            human_score+=1;
         }
 
-        else{
-            console.log("rock loses to scissors");
+        else if(human_choice==="scissors"){
+            console.log("scissors loses to rock");
             console.log("you lost");
-            computer+=1;
+            vs.innerText="loses to"
+            computer_score+=1;
         }
     }
 
@@ -40,18 +64,21 @@ function play_game(computer_choice,human_choice){
         if(human_choice=="rock"){
             console.log("rock loses to paper");
             console.log("you lost");
-            computer+=1;
+            vs.innerText="loses to"
+            computer_score+=1;
         }
 
-        else if(human_choice="paper"){
+        else if(human_choice==="paper"){
             console.log("paper and paper");
-            console.log("match tied");            
+            console.log("match tied");  
+            vs.innerText="tie"          
         }
 
-        else{
+        else if(human_choice==="scissors"){
             console.log("scissors beats paper");
             console.log("you won");
-            human+=1;
+            vs.innerText="beats"
+            human_score+=1;
         }
     }
 
@@ -60,42 +87,70 @@ function play_game(computer_choice,human_choice){
         if(human_choice=="rock"){
             console.log("rock beats scissors");
             console.log("you won");
-            human+=1;
+            vs.innerText="beats"
+            human_score+=1;
         }
 
-        else if(human_choice="paper"){
+        else if(human_choice==="paper"){
             console.log("paper loses to scissors");
             console.log("you lost")
-            computer+=1;
+            vs.innerText="loses"
+            computer_score+=1;
         }
 
-        else{
+        else if(human_choice==="scissors"){
             console.log("scissors and scissors");
             console.log("match tied");
+            vs.innerText="tie"
         }
     }
 }
+
+const round_no=document.getElementById("round");
 
 console.log("game started");
 
-for(let i=0; i<5;i++){
 
-    console.log("match",i+1);
+async function main(){
+    for(let i=1; i<=5;i++){
 
-    play_game(computer_choice(),human_choice());
-    console.log("computer score",computer);
-    console.log("huaman score",human);
+        await sleep(3000);
+        round_no.innerText=`Round ${i}`;    
 
-}
+        console.log("match",i);
 
-if(computer>human){
-        console.log("you lost the game");
+        const human=human_choice();
+        const computer=computer_choice();
+
+        changing_images(computer,human);
+
+        play_game(computer,human);
+
+        console.log("computer score",computer_score);
+        console.log("human score",human_score);
+
+        document.getElementById('human_score').innerText=`Your score: ${human_score}`;
+        document.getElementById('computer_score').innerText=`Computer score: ${computer_score}`;
+
+        
+
     }
 
-    else if(human>computer){
-        console.log("you won the game");
-    }
 
-    else{
-        console.log("game tied");
+    if(computer_score<human_score){
+            document.getElementById('final_result').innerText="Congratulations You won"
+            console.log("you won the game");            
+        }
+
+        else if(human_score<computer_score){
+            document.getElementById('final_result').innerText="Sadly You lost"
+            console.log("you lost the game");
+        }
+
+        else{
+            document.getElementById('final_result').innerText="Game tied"
+            console.log("game tied");
+    }
 }
+
+main();
